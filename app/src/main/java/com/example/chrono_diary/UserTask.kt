90bc5afa1b654bd4636166bd51cc802b51fbe55a
Pedro Queiroga <pedro.00000847553@unicap.br>
2024-09-20 18:33:19 -0300

    user usertask

--- app/src/main/java/com/example/chrono_diary/UserTask.kt
+++ app/src/main/java/com/example/chrono_diary/UserTask.kt
@@ -1,4 +1,12 @@
 package com.example.chrono_diary
 
-class UserTask {
-}
+import com.example.loginapp.User
+
+data class UserTask (
+    val taskname: String,
+    val descricion: String,
+    val startdate: String,
+    val Starttime: String,
+    val endtime: String,
+    val tag: String
+)
