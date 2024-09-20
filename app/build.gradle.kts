6cd50fa8b52cb0a77840acf93f45446f8002e543
Samuelj-santos <samuel.00000847429@unicap.br>
2024-09-20 10:04:40 -0300

    Cor do calendario fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.material.v100)
+    implementation(libs.material)
 }
